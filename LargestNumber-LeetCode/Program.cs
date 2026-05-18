edf84bccd978f320c3528def6bbbc3cf321308cd
LENOVO <LENOVO@DESKTOP-KOAIK61>
2026-05-18 06:35:43 +0300

    add solution largest number leet code

---
+++ LargestNumber-LeetCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+public class Solution
+{
+    public string LargestNumber(int[] nums)
+    {
+        //Bubble Sort Idea
+        var list = new List<string>();
+        foreach (var item in nums)
+        {
+            list.Add(item.ToString());
+        }
+        list.Sort((a, b) => (b + a).CompareTo(a + b));
+        if (list[0] == "0") return "0";
+
+        var result = string.Empty;
+        foreach (var item in list)
+        {
+            result += item;
+        }
+        return result;
+
+    }
+
+
+}
+
+
+class program
+{
+    static async Task Main()
+    {
+
+        var objec = new Solution();
+    }
+}
